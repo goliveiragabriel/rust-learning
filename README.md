bbc711527973b960c636ea11215c6e378c494f3d
goliveiragabriel <goliveiragabriel@gmail.com>
2021-08-29 09:35:50 -0300

    Added unit test and refactored main.rs

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 This is a learning project created using Cargo Package Manager. To create a new package use the command: `cargo new samples`.
 
 ## How to run
-Navigate to *samples* folder and run the commands `cargo build` then `cargo run`  
+Navigate to **samples** folder and run the commands `cargo build` then `cargo run`  
+
+## How to run test
+Navigate to **samples** folder and run the command `cargo test`
--- samples/src/main.rs
+++ samples/src/main.rs
@@ -1,3 +1,21 @@
+
 fn main() {
-    println!("Hello {firstName} {lastName}", firstName="Gabriel", lastName="Gonçalves");
+    println!("{}", print_message("Gabriel".to_string(), "Gonçalves".to_string()));
+}
+
+pub fn print_message(first_name: String, last_name: String) -> String {
+    format!("Hello {firstName} {lastName}", firstName=first_name, lastName=last_name)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::print_message;
+    
+    #[test]
+    fn should_return_full_name() {
+        let first_name = "Gabriel";
+        let last_name = "Gonçalves";
+        let result = print_message(first_name.to_string(), last_name.to_string());
+        assert_eq!("Hello Gabriel Gonçalves", result);
+    }
 }
